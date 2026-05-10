a7fcfca517bc591a4e0198aa4069515a62baa6fd
SaiPrashanthP <digitalrevoltindia@gmail.com>
2026-05-10 22:51:05 +0530

    Connected frontend with Railway backend

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://backendapi-production-3b68.up.railway.app',
 });
 
 api.interceptors.request.use((config) => {
